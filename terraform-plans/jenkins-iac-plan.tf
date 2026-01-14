64d7d90228e7f67ca193f8158d6df32c2858daaa
Joshua Williams <jw2025-01@outlook.com>
2026-01-13 22:25:30 -0500

    chore(): add provider resources.

--- terraform-plans/jenkins-iac-plan.tf
+++ terraform-plans/jenkins-iac-plan.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.57"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
 resource "azurerm_resource_group" "jenkins_poc" {
   name     = "rg-jenkins-demo"
   location = "East US"
